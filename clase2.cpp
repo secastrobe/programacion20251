4652f2be41ecb53a63c50dec6439909bd36f5bdc
secastrobe <secastrobe@unal.edu.co>
2025-04-09 05:37:20 -0500

    Codigo primera semana

---
+++ clase2.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+#include <string>
+
+int main(){
+    std::string nombre;
+    std::cout <<"Ingrse su nombre:";
+    std::cin >> nombre;
+    std::cout <<"\nHola"<< nombre <<"\n";
+    return 0;
+}
---
+++ hola.cpp
@@ -0,0 +1,6 @@
+# include <iostream>
+
+int main(void){
+    std::cout << "Hola mundo\n";
+    return 0;
+}
