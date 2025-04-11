1e048623c70a07a97b766652aa33e0a709e2f29f
secastrobe <secastrobe@unal.edu.co>
2025-04-11 08:39:14 -0500

    Ahora el juego esta en una funcion

--- guess_game.cpp
+++ guess_game.cpp
@@ -7,22 +7,35 @@ Informar si se adivinó el número o no
 */
 #include <iostream>
 
-int main(void) {
+//declaración de la funcion de juego
+
+void play (void){
     const int num = 10;
-    int guessed_number = num+1;
+    int guessed_number = num+1; // Asigna el numero adivinado para que while se ejecute ṕor primera vez
 
     while(guessed_number != num){
         std::cout << "Adivina un numero entre 1 y 100\n";
         std::cin >> guessed_number; //Asigna el número adivinado a la variable
-  
-        if (guessed_number == num) {
-            std::cout << "Ganaste!\n";
-        }   else  if (guessed_number > num ) {
-            std::cout<<"Te pasaste\n";
-        }   else {
-            std::cout<<"Te falto\n";
+
+        std::cout << "Escribiste el numero:" << guessed_number << "\n";  
+        if(guessed_number <= 1 || guessed_number >= 100){ //Comprueba si el numero esta en el rango permitido
+            std::cout<< "El numero que ingresate no esta dentro de el rango permitido, por favor vuelve a intentar\n";
+            continue;
+        }
+        else {
+            if (guessed_number == num) {
+                std::cout << "Ganaste!\n";
+            }   else  if (guessed_number > num ) {
+                std::cout<<"Te pasaste\n";
+            }   else {
+                std::cout<<"Te falto\n";
+            }
         }
     }
+}
+
+int main(void) {
+    play();
     return 0;
 }   
 
