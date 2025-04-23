f7178e1e735c5307e449f9a2fbd4830399bfd685
secastrobe <secastrobe@unal.edu.co>
2025-04-23 07:16:41 -0500

    Trabajos hasta el 23 de abril

--- aprox_pi.cpp
+++ aprox_pi.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 
+//redefinimos pi para evitar problemas con cmath
+#ifndef M_PI 
+const double M_PI = 3.14159265358979323846;
+#endif
 
-
-float pi_aprox(int numero){
-    double pi_apx = 0;
+double pi_aprox(int numero){
+    double pi_apx = 0; //Hace un reset al valor de pi para volver a calcular
     for (int i = 0; i < numero; i++)
     {
-        double potencia = pow(16,i);
-        double mult = ((4.0/((8*i) + 1))-(2.0/((8*i) + 4))-(1.0/((8*i) + 5))-(1.0/((8*i) + 6)));
-        pi_apx += (1/potencia)*mult;
-        //std::cout << "\nPotencia:" << potencia << "\ntermino:" << mult;
+        double mult = ((4.0/((8*i) + 1))
+                        -(2.0/((8*i) + 4))
+                        -(1.0/((8*i) + 5))
+                        -(1.0/((8*i) + 6)));
+        pi_apx += mult/std::pow(16,i); //divide el termino por la potencia de 16
     }
     return pi_apx;
 }
 int main(void){
-    int num;
-    while(true){
-        std::cout << "Ingrese el numero de terminos que desea tenga la aproximacion de pi (entre 1 y 20 terminos) : ";
-        std::cin >> num;
-        std ::cout << num;
-        if (num < 1 or num > 20){
-            std::cout << "\nEl numero no esta dentro del rango permitido, intentalo de nuevo:" << "\n";
-            continue;
-        }
-        float pi = pi_aprox(num);
-        std::cout << "\n" << "La aproximacion de pi es:" << pi << "\n";
-        break;
+    std::cout.precision(16);
+    std::cout.setf(std::ios::scientific); //Configura notacion cientifica
+    int n = 20;
+    for(int i=1; i<=n; i++){ //realiza n veces el ciclo
+        double pi = pi_aprox(i);
+        double dif = (M_PI - pi)/pi;
+        double diferencia = std::abs(dif);
+        std::cout << "\n" << "La aproximacion de pi es: " << pi << "\n";
+        std::cout << "El valor de n usado fue: " << i << "\n";
+        std::cout << "La diferencia relativa es: " << diferencia << "\n";
     }
     return 0;
 }
---
+++ juegoguess.cpp
@@ -0,0 +1,42 @@
+/*
+Juego de adivinar un numero
+
+Tener un numero
+Preguntar al usuario el numero
+Informar si lo adivino o no
+
+*/
+
+#include <iostream>
+
+int main(void) {
+
+    const int NUM = 10;
+    int guessed_number = NUM/2;
+
+    const int MIN = 1;
+    const int MAX = 100;
+
+    while(guessed_number != NUM) {
+        std::cout << "Adivina un numero entre  " << MIN << "  y " << MAX << ":\n";
+        std::cin >> guessed_number;
+        std::cout << "Ecribiste: " << guessed_number << "\n";
+
+        // validacion del rango 
+        if (guessed_number < MIN or MAX < guessed_number) {
+            std::cout << "El numero no esta en el rango.\n";
+            continue; 
+        }
+
+        if (guessed_number == NUM) {
+            std::cout << "GANASTE!!!!\n";
+            //break;
+        } else if (guessed_number > NUM){
+            std::cout << "Te pasaste.\n";
+        } else { // es menor
+            std::cout << "Te falta.\n";
+        }
+    }
+
+    return 0;
+}
