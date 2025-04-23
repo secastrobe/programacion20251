4aa82ace77ac2f3408a4690c054cc5be584d99e1
secastrobe <secastrobe@unal.edu.co>
2025-04-23 07:34:53 -0500

    Ejercicio numero primo

---
+++ 23-04/primo.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+bool is_prime(int n){
+    for(int i = n - 1; i>1; i--){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(void){
+    int num = 0;
+    bool primo = true;
+    while(true){
+        int num = 0;
+        std::cout << "write your number: ";
+        std::cin >> num;
+        if(num < 1){
+            std::cout<<"Your number should be grater than 1, try again\n";
+            continue;
+        }else{
+        primo = is_prime(num);
+        break;
+        }
+    }
+    if(primo){
+        std::cout<<"\n Your number is prime\n";
+    }else{
+        std::cout<<"\n Yout number is not prime\n";
+    }
+    
+}
